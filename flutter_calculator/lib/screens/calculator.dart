6e2adcf96895ad7beec95e3c478146d4fd543e74
Luís Eduardo Marinho Fernandes <duduyugi@gmail.com>
2021-04-21 11:43:20 -0300

    Adiciona dependencias, keyboard e display

--- flutter_calculator/lib/screens/calculator.dart
+++ flutter_calculator/lib/screens/calculator.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../components/display.dart';
+import '../components/keyboard.dart';
 
 class Calculator extends StatelessWidget {
   @override
@@ -6,8 +8,8 @@ class Calculator extends StatelessWidget {
     return MaterialApp(
         home: Column(
           children: <Widget>[
-            Text('Display'),
-            Text('Keyboard'),
+            Display('123.45'),
+            Keyboard(),
           ],
         ),
     );
