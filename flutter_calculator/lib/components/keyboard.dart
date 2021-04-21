6b0cf0c2874dd86e346eda52abb961407c2b3816
Luís Eduardo Marinho Fernandes <duduyugi@gmail.com>
2021-04-21 12:19:17 -0300

    adiciona button row button e visual do display

--- flutter_calculator/lib/components/keyboard.dart
+++ flutter_calculator/lib/components/keyboard.dart
@@ -1,10 +1,44 @@
 import 'package:flutter/material.dart';
+import 'button.dart';
+import 'button_row.dart';
 
 class Keyboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 500,
+      child: Column(
+        children: <Widget>[
+          ButtonRow([
+            Button.big(text: 'AC'),
+            Button(text: '%'),
+            Button.operation(text: '/'),
+          ]),
+          ButtonRow([
+            Button(text: '7'),
+            Button(text: '8'),
+            Button(text: '9'),
+            Button.operation(text: 'x'),
+          ]),
+          ButtonRow([
+            Button(text: '4'),
+            Button(text: '5'),
+            Button(text: '6'),
+            Button.operation(text: '-'),
+          ]),
+          ButtonRow([
+            Button(text: '1'),
+            Button(text: '2'),
+            Button(text: '3'),
+            Button.operation(text: '+'),
+          ]),
+          ButtonRow([
+            Button.big(text: '0', big: true),
+            Button(text: ','),
+            Button.operation(text: '='),
+          ]),
+        ],
+      ),
     );
   }
 }
